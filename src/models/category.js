8bd89d8973b9afa66355e3fc4dbae4ca34cc5270
hatem <hatim2012003@gmail.com>
2025-06-26 11:58:00 +0300

    Update

--- src/models/category.js
+++ src/models/category.js
@@ -18,6 +18,6 @@ const categorySchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
-const Category = mongoose.model('category', categorySchema);
+const CategoryModel = mongoose.model('category', categorySchema);
 
-export default Category;
+export default CategoryModel;
